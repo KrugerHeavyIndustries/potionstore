832fbf371ccfbfb5cf08e4eba290db8c2f486be9
Chris Kruger <chris.kruger@mokosocialmedia.com>
2017-04-11 15:44:11 +0800

    updated capistrano configuration to link config/pin_payment.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/var/rails/potionstore"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/store.yml", "config/paypal.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/store.yml", "config/paypal.yml", "config/pin_payment.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
